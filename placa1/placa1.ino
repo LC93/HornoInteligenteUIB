8787c81899c743190b4ee52f0822c295e88778df
BarkSanson <barksansondev@gmail.com>
2023-05-10 15:53:16 +0200

    Initial commit

---
+++ placa1/placa1.ino
@@ -0,0 +1,25 @@
+void taskGrill() {
+  
+}
+
+void taskSimTemp() {
+  
+}
+
+void taskSensorTemp() {
+  
+}
+
+void taskControlTemp() {
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
