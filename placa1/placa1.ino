5e45a2854f9692d2659334967315af45f1cd18b8
BarkSanson <barksansondev@gmail.com>
2023-05-10 17:31:40 +0200

    Empezada función de simulación

--- placa1/placa1.ino
+++ placa1/placa1.ino
@@ -5,6 +5,10 @@
 HIB hib;
 SO so;
 
+#define MINIMUM_ADC_VALUE 0
+#define MAXIMUM_ADC_VALUE 1023
+#define MINIMUM_SLOPE_VALUE 50
+#define MAXIMUM_SLOPE_VALUE 150
 
 /***************************
   Declaration of semaphores
@@ -27,7 +31,7 @@ const unsigned char maskAdcEvent = 0x01;
 /**************************
   Declaration of global variables
 ***************************/
-volatile uint8_t tGrill;
+volatile uint8_t tGrill = 100;
 volatile float sampledTempOven;
 volatile uint16_t slopeTemp;
 
@@ -37,7 +41,6 @@ volatile uint16_t slopeTemp;
 void adcHook(uint16_t newAdcValue) {
   slopeTemp = newAdcValue;
   so.setFlag(f_adc, maskAdcEvent);
-
 }
 
 void taskGrill() {
@@ -45,16 +48,44 @@ void taskGrill() {
 }
 
 void taskSimTemp() {
+  uint16_t tOven;
+  long mappedTOven;
+
   while (1) {
     so.waitFlag(f_adc, maskAdcEvent);
     so.clearFlag(f_adc, maskAdcEvent);
-    hib.ledToggle(1);
+
+    mappedTOven = map(
+      slopeTemp, 
+      MINIMUM_ADC_VALUE, 
+      MAXIMUM_ADC_VALUE, 
+      MINIMUM_SLOPE_VALUE, 
+      MAXIMUM_SLOPE_VALUE
+    );
+
+    tOven = tGrill * slopeTemp;
+
+    // Serial.print("Temperatura horno: "); Serial.println(tOven); // DEBUG
+    // Serial.flush(); //DEBUG
+
+    so.signalMBox(mb_tOven, (byte *) &tOven);
+
   }
 
 
 }
 
 void taskSensorTemp() {
+  float tOven;
+  float *tOvenMessage;
+  
+  so.waitMBox(mb_tOven, (byte**) &tOvenMessage);
+  
+  //Serial.print("Temperatura horno en sensor: "); Serial.println(*tOvenMessage); // DEBUG
+  //Serial.flush(); // DEBUG
+
+  tOven = *tOvenMessage;
+  hib.ledToggle(1);
 
 }
 
@@ -74,8 +105,9 @@ void loop() {
   mb_tOven = so.defMBox();
   f_adc = so.defFlag();
 
-  hib.adcSetTimerDriven(TIMER_TICKS_FOR_250ms, TIMER_PSCALER_FOR_250ms, adcHook);
-  so.defTask(taskSimTemp, 1);
+  hib.adcSetTimerDriven(TIMER_TICKS_FOR_500ms, TIMER_PSCALER_FOR_500ms, adcHook);
+  so.defTask(taskSimTemp, 2);
+  so.defTask(taskSensorTemp, 1);
 
   so.enterMultiTaskingEnvironment();
 }
