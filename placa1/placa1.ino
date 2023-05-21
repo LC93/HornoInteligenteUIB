52020f3b1f3bf73644d46464cb20700a2d4e66ab
BarkSanson <barksansondev@gmail.com>
2023-05-21 12:51:25 +0200

    Todas las tareas de temperatura funcionan bien

--- placa1/placa1.ino
+++ placa1/placa1.ino
@@ -4,6 +4,8 @@
 
 #include "temperature.h"
 
+#define PERIOD_CONTROL_TEMP_TASK 4;
+
 HIB hib;
 SO so;
 
@@ -29,16 +31,14 @@ const unsigned char maskAdcEvent = 0x01;
 // to control taskGrill
 Flag f_temp;
 const unsigned char maskGrillOff = 0x01;
-const unsigned char maskGrillLow = 0x02;
-const unsigned char maskGrillMedium = 0x04;
-const unsigned char maskGrillHigh = 0x08;
+const unsigned char maskGrillOn = 0x02;
 
 
 /**********************************
   Declaration of global variables
 **********************************/
-volatile uint8_t tGrill = TGRILL_ON;
-volatile float sampledTempOven;
+volatile uint8_t tGrill = TGRILL_OFF;
+volatile uint16_t sampledTempOven;
 volatile uint16_t slopeTemp;
 
 /*****************
@@ -49,11 +49,18 @@ void adcHook(uint16_t newAdcValue) {
   so.setFlag(f_adc, maskAdcEvent);
 }
 
+/*****************
+  Timer 5 handling
+*****************/
+void timer5Hook() {
+  so.updateTime();
+}
+
 /*****************************
   Temperature-realted tasks
 *****************************/
 void taskSimTemp() {
-  int16_t tOven = 0;
+  uint16_t tOven = 0;
   float mappedSlope;
 
   while (1) {
@@ -66,24 +73,27 @@ void taskSimTemp() {
                       MINIMUM_SLOPE_VALUE,
                       MAXIMUM_SLOPE_VALUE
                      );
-    Serial.print("Temperatura horno preCalc: "); Serial.println(tOven);
+    //Serial.print("Temperatura horno preCalc: "); Serial.println(tOven);
 
+    so.waitSem(s_tGrill);
     if (tGrill == TGRILL_OFF)
       mappedSlope = -mappedSlope;
+    so.signalSem(s_tGrill);
 
-    tOven = round((float)tGrill * mappedSlope / 250.0) + tOven; // Funcion que simula la temperatura.
+    tOven = mappedSlope + tOven; // Funcion que simula la temperatura.
 
     if (tOven < 0)  // Limita la temperatura mínima simulada.
       tOven = 0;
-    if(tOven > TGRILL_ON) // Limita la temperatura máxima simulada.
+    if (tOven > TGRILL_ON) // Limita la temperatura máxima simulada.
       tOven = TGRILL_ON;
 
-    Serial.print("mapped Slope: "); Serial.println(mappedSlope);
-    Serial.print("tGrill: "); Serial.println(tGrill);
-    Serial.print("Temperatura horno: "); Serial.println(tOven); // DEBUG
-    Serial.flush(); //DEBUG
+    //Serial.print("mapped Slope: "); Serial.println(mappedSlope);
+    //Serial.print("tGrill: "); Serial.println(tGrill);
+    //Serial.print("  Temperatura horno simulada: "); Serial.println(tOven); // DEBUG
+    //Serial.flush(); //DEBUG
 
     so.signalMBox(mb_tOven, (byte *) &tOven);
+    hib.ledToggle(1); // DEBUG
 
   }
 }
@@ -101,16 +111,72 @@ void taskSensorTemp() {
     //Serial.print("Temperatura horno en sensor: "); Serial.println(tOven); // DEBUG
     //Serial.flush(); // DEBUG
 
-    // hib.ledToggle(1); // DEBUG
+    so.waitSem(s_tempOven);
+
+    sampledTempOven = tOven;
+
+    so.signalSem(s_tempOven);
+
+    hib.ledToggle(2); // DEBUG
   }
 }
 
 void taskControlTemp() {
+  unsigned long nextActivationTick;
+  uint16_t tOven;
+
+  nextActivationTick = so.getTick();
+
+  while (1) {
+    so.waitSem(s_tempOven);
+    tOven = sampledTempOven;
+    so.signalSem(s_tempOven);
+
+    // Esto es un if de prueba, pero la temperatura
+    // de referencia vendrá dada por la consigna
+    so.waitSem(s_tGrill);
+    Serial.print("Control ve grill: "); Serial.println(tGrill);
+    if (tOven >= 50 && s_tGrill != TGRILL_OFF) {
+      //Serial.println("Control apaga grill");
+      so.setFlag(f_temp, maskGrillOff);
+    } else if (tOven < 50 && s_tGrill != TGRILL_ON) {
+      //Serial.println("Control enciende grill");
+      so.setFlag(f_temp, maskGrillOn);
+    }
+    so.signalSem(s_tGrill);
+
+    Serial.print("Temperatura horno desde control: "); Serial.println(tOven); // DEBUG
+    Serial.flush(); // DEBUG
+    hib.ledToggle(3); // DEBUG
+
+    nextActivationTick = nextActivationTick + PERIOD_CONTROL_TEMP_TASK;
+    so.delayUntilTick(nextActivationTick);
+  }
 
 }
 
 void taskGrill() {
+  unsigned char mask = (maskGrillOff | maskGrillOn);
+  unsigned char flagValue;
 
+  while (1) {
+    so.waitFlag(f_temp, mask);
+    flagValue = so.readFlag(f_temp);
+    so.clearFlag(f_temp, mask);
+
+    so.waitSem(s_tGrill);
+    Serial.print("Grill se ve: "); Serial.println(tGrill);
+    if (flagValue == maskGrillOff && tGrill != TGRILL_OFF) {
+      Serial.println("Grill se apaga"); // DEBUG
+      tGrill = TGRILL_OFF;
+    } else if (flagValue == maskGrillOn && tGrill != TGRILL_ON) {
+      Serial.println("Grill se enciende"); // DEBUG
+      tGrill = TGRILL_ON;
+    }
+    so.signalSem(s_tGrill);
+
+    hib.ledToggle(4); // DEBUG
+  }
 }
 
 /************************
@@ -145,10 +211,13 @@ void loop() {
   f_adc = so.defFlag();
   f_temp = so.defFlag();
 
+  hib.setUpTimer5(TIMER_TICKS_FOR_125ms, TIMER_PSCALER_FOR_125ms, timer5Hook);
   hib.adcSetTimerDriven(TIMER_TICKS_FOR_500ms, TIMER_PSCALER_FOR_500ms, adcHook);
 
-  so.defTask(taskSimTemp, 1);
-  so.defTask(taskSensorTemp, 2);
+  so.defTask(taskControlTemp, 1);
+  so.defTask(taskSimTemp, 2);
+  so.defTask(taskSensorTemp, 3);
+  so.defTask(taskGrill, 4);
 
   so.enterMultiTaskingEnvironment();
 }
